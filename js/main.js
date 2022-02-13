df1930c4b3c2328467a084070832cd15f87e654d
Svyatoslav <svyatoslavlp@mail.ru>
2022-02-13 11:55:30 +0500

    пилим слайдер 80%

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+   slidesPerView: 2,
+   spaceBetween: 30,
+   // Optional parameters
+   loop: true,
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.main__series-arrow',
+   },
+
+});
